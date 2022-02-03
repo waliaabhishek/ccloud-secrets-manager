4cbe419121cff3682dab0d5d6641a840b790ba25
Abhishek Walia <abhishek@confluent.io>
2022-02-02 16:22:12 -0800

    CCloud Env and Cluster Class structure setup

--- clusters.py
+++ clusters.py
@@ -1,77 +1,136 @@
-import pprint
+from typing import Dict
 from urllib import parse
 
 import requests
 
 import base_ccloud
 
-pp = pprint.PrettyPrinter(indent=2)
-ENV_VALUES = {}
-CLUSTER_VALUES = {}
+
+class CCloudEnvironment:
+    def __init__(self, env_id: str, display_name: str, created_at: str) -> None:
+        self.env_id = env_id
+        self.display_name = display_name
+        self.created_at = created_at
 
 
-def cache_environments(url, params, value_dict):
-    resp = requests.get(url=url, auth=base_ccloud.BASIC_AUTH, params=params)
-    if resp.status_code == 200:
-        out_json = resp.json()
-        for item in out_json["data"]:
-            value_dict[item["id"]] = item
-        if 'next' in out_json['metadata']:
-            query_params = parse.parse_qs(
-                parse.urlsplit(out_json['metadata']['next']).query)
-            params['page_token'] = str(query_params['page_token'][0])
-            cache_clusters(url=url, params=params, value_dict=value_dict)
-    else:
-        raise Exception("Could not connect to Confluent Cloud. Please check your settings. "
-                        + resp.text)
+class CCloudEnvironmentList:
+    env: Dict[str, CCloudEnvironment]
+
+    def __init__(self, ccloud_connection: base_ccloud.CCloudConnection) -> None:
+        uri = base_ccloud.URIDetails()
+        self.env_url = uri.get_endpoint_url(uri.environments)
+        self.auth = ccloud_connection.api_http_basic_auth
+        self.env = {}
+        print("Gathering list of all environment(s) in CCloud.")
+        self.read_all_env()
+
+    def __str__(self):
+        print("Found " + str(len(self.env)) + " environments.")
+        for v in self.env.values():
+            print("{:<15} {:<40}".format(v.env_id, v.display_name))
+
+    def read_all_env(self, params={"page_size": 50}):
+        resp = requests.get(url=self.env_url, auth=self.auth, params=params)
+        if resp.status_code == 200:
+            out_json = resp.json()
+            for item in out_json["data"]:
+                print("Found environment " + item["id"] + " with name " + item["display_name"])
+                self.__add_env_to_cache(item["id"], item["display_name"], item["metadata"]["created_at"])
+            if "next" in out_json["metadata"]:
+                query_params = parse.parse_qs(parse.urlsplit(out_json["metadata"]["next"]).query)
+                params["page_token"] = str(query_params["page_token"][0])
+                self.read_all_env(params)
+        else:
+            raise Exception("Could not connect to Confluent Cloud. Please check your settings. " + resp.text)
+
+    def __add_env_to_cache(self, env_id: str, display_name: str, created_at: str) -> CCloudEnvironment:
+        self.env[env_id] = CCloudEnvironment(env_id, display_name, created_at)
+        return self.env[env_id]
+
+    # Read/Find one Cluster from the cache
+    def find_environment(self, env_id):
+        return self.env[env_id]
 
 
-def print_env_table(val_dict):
-    for k, v in val_dict.items():
-        print("{:<15} {:<40}".format(
-            v["id"], v["display_name"]))
+class CCloudCluster:
+    def __init__(
+        self,
+        env_id: str,
+        cluster_id: str,
+        cluster_name: str,
+        cloud: str,
+        availability: str,
+        region: str,
+        bootstrap_url: str,
+    ) -> None:
+        self.env_id = env_id
+        self.cluster_id = cluster_id
+        self.cluster_name = cluster_name
+        self.cloud = cloud
+        self.availability = availability
+        self.region = region
+        self.bootstrap_url = bootstrap_url
 
 
-def cache_clusters(url, params, value_dict):
-    resp = requests.get(url=url, auth=base_ccloud.BASIC_AUTH, params=params)
-    if resp.status_code == 200:
-        out_json = resp.json()
-        for item in out_json["data"]:
-            value_dict[item["id"]] = item
-        if 'next' in out_json['metadata']:
-            query_params = parse.parse_qs(
-                parse.urlsplit(out_json['metadata']['next']).query)
-            params['page_token'] = str(query_params['page_token'][0])
-            cache_clusters(url=url, params=params, value_dict=value_dict)
-    else:
-        raise Exception("Could not connect to Confluent Cloud. Please check your settings. "
-                        + resp.text)
+class CCloudClusterList:
+    cluster: Dict[str, CCloudCluster]
 
+    def __init__(self, ccloud_connection: base_ccloud.CCloudConnection, ccloud_env: CCloudEnvironmentList) -> None:
+        uri = base_ccloud.URIDetails()
+        self.cluster_url = uri.get_endpoint_url(uri.clusters)
+        self.auth = ccloud_connection.api_http_basic_auth
+        self.cluster = {}
+        print("Gathering list of all Cluster(s) in every environment in CCloud.")
+        for item in ccloud_env.env.values():
+            print("Checking Environment " + item.env_id + " for any provisioned clusters.")
+            self.read_all_clusters(item.env_id)
 
-def print_cluster_table(val_dict):
-    for v in val_dict.values():
-        print("{:<15} {:<15} {:<25} {:<10} {:<25} {:<50}".format(
-            v["spec"]["environment"]["id"], v["id"], v["spec"]["display_name"], v["spec"]["cloud"], v["spec"]["availability"], v["spec"]["kafka_bootstrap_endpoint"]))
+    def __str__(self):
+        for v in self.cluster.values():
+            print(
+                "{:<15} {:<15} {:<25} {:<10} {:<25} {:<50}".format(
+                    v.env_id, v.cluster_id, v.cluster_name, v.cloud, v.availability, v.bootstrap_url
+                )
+            )
 
+    def read_all_clusters(self, env_id: str, params={"page_size": 50}):
+        params["environment"] = env_id
+        resp = requests.get(url=self.cluster_url, auth=self.auth, params=params)
+        if resp.status_code == 200:
+            out_json = resp.json()
+            for item in out_json["data"]:
+                print("Found cluster " + item["id"] + " with name " + item["spec"]["display_name"])
+                self.__add_cluster_to_cache(
+                    env_id,
+                    item["id"],
+                    item["spec"]["display_name"],
+                    item["spec"]["cloud"],
+                    item["spec"]["availability"],
+                    item["spec"]["region"],
+                    item["spec"]["kafka_bootstrap_endpoint"],
+                )
+            if "next" in out_json["metadata"]:
+                query_params = parse.parse_qs(parse.urlsplit(out_json["metadata"]["next"]).query)
+                params["page_token"] = str(query_params["page_token"][0])
+                self.read_all_clusters(env_id, params)
+        else:
+            raise Exception("Could not connect to Confluent Cloud. Please check your settings. " + resp.text)
 
-def run_cluster_workflow():
-    cache_environments(
-        base_ccloud.CCLOUD_URL + base_ccloud.URI_LIST['environment'], {"page_size": 20}, value_dict=ENV_VALUES)
-    print("Found " + str(len(ENV_VALUES)) + " environments.")
-    # print("=" * 80)
-    # print_env_table(ENV_VALUES)
-    # print("=" * 80)
+    def __add_cluster_to_cache(
+        self,
+        env_id: str,
+        cluster_id: str,
+        cluster_name: str,
+        cloud: str,
+        availability: str,
+        region: str,
+        bootstrap_url: str,
+    ) -> CCloudCluster:
+        self.cluster[cluster_id] = CCloudCluster(
+            env_id, cluster_id, cluster_name, cloud, availability, region, bootstrap_url
+        )
+        return self.cluster[cluster_id]
 
-    for env in ENV_VALUES:
-        cache_clusters(base_ccloud.CCLOUD_URL + base_ccloud.URI_LIST['clusters'],
-                       {"environment": env, "page_size": 20}, value_dict=CLUSTER_VALUES)
-    # print("\n\n")
-    print("Found " + str(len(CLUSTER_VALUES)) + " clusters.")
-    # print("=" * 80)
-    # print_cluster_table(CLUSTER_VALUES)
-    # print("=" * 80)
-
-
-if __name__ == '__main__':
-    base_ccloud.initial_setup(None)
-    run_cluster_workflow()
+    # Read/Find one Cluster from the cache
+    def find_cluster(self, cluster_id):
+        return self.cluster[cluster_id]
