84d2f8d3e429a4e447ad2a2c505b704e5f37e54f
Abhishek Walia <abhishek.walia@gmail.com>
2022-03-02 20:25:03 -0800

    Clean remnant code. Add Create Update logic for RP User

--- aws_secrets_manager.py
+++ aws_secrets_manager.py
@@ -200,6 +200,7 @@ class AWSSecretsList(CSMSecretsList):
         csm_configs: CSMConfig,
         ccloud_api_key_list: CCloudAPIKeyList,
         ccloud_cluster_list: CCloudClusterList,
+        ccloud_env_list: CCloudEnvironmentList,
         ccloud_sa_list: CCloudServiceAccountList,
         new_api_keys: List[CCloudAPIKey] = None,
         **kwargs,
@@ -235,252 +236,37 @@ class AWSSecretsList(CSMSecretsList):
             rp_secret = self.get_secret(rp_secret_name)
             if rp_secret:
                 rp_secret_data = loads(rp_secret["SecretString"])
+                is_new_secret = False
             else:
                 rp_secret_data = ""
+                is_new_secret = True
             update_triggered = False
             for api_key in [v for v in new_api_keys if v.cluster_id == item]:
                 # Rest proxy Front End String Update
                 is_updated, rp_secret_data["basic.txt"] = self.__add_front_end_user_to_rp_secret_string(
-                    rp_secret_name, rp_secret_data["basic.txt"], api_key.api_key, api_key.api_secret
+                    rp_secret_name, rp_secret_data.get("basic.txt", ""), api_key.api_key, api_key.api_secret
                 )
                 if is_updated:
                     update_triggered = True
                 is_updated, rp_secret_data["restProxyUsers.jaas"] = self.__add_kafka_user_to_rp_secret_string(
-                    rp_secret_name, rp_secret_data["restProxyUsers.jaas"], api_key.api_key, api_key.api_secret
+                    rp_secret_name, rp_secret_data.get("restProxyUsers.jaas", ""), api_key.api_key, api_key.api_secret
                 )
                 if is_updated:
                     update_triggered = True
             if update_triggered:
-                response = client.put_secret_value(SecretId=rp_secret_name, SecretString=dumps(rp_secret_data))
-                print(f"Secret Successfully updated. Response\n {response}")
-
-
-def add_secrets_to_rest_proxy_user(
-    env_id: str,
-    cluster_id: str,
-    secret_name_prefix: str,
-    api_key: str,
-    api_secret: str,
-    rp_fe_user_filepath: str,
-    secret_tags: dict,
-    rest_proxy_user="rest-proxy-user",
-    proxy_user_string="krp-users",
-):
-    rest_proxy_secret_name = create_secret_name(env_id, cluster_id, rest_proxy_user, api_key, secret_name_prefix)
-    secret_data = get_secret(rest_proxy_secret_name)
-    secret_tags.pop("sa_name", None)
-    secret_tags.pop("sa_id", None)
-    secret_tags.pop("allowed_in_rest_proxy", None)
-    secret_tags["rest_proxy_user"] = "True"
-    if not secret_data:
-        print("Did not find any Rest Proxy Secret for the environment and cluster. Creating a new Secret now.")
-        basic_user = api_key + ": " + api_secret + "," + proxy_user_string
-        jaas_user = "\n".join(
-            [
-                "KafkaRest {",
-                "\torg.eclipse.jetty.jaas.spi.PropertyFileLoginModule required",
-                '\tdebug="true"',
-                str('\tfile="' + rp_fe_user_filepath + '";'),
-                "};",
-                "" "KafkaClient {",
-                "\torg.apache.kafka.common.security.plain.PlainLoginModule required",
-                str('\tusername="' + api_key + '"'),
-                str('\tpassword="' + api_secret + '";'),
-                "};",
-            ]
-        )
-        secret_data = {
-            "basic.txt": basic_user,
-            "restProxyUsers.jaas": jaas_user,
-        }
-        create_secret(rest_proxy_secret_name, secret_data, render_secret_tags_format(secret_tags))
-    else:
-        secret_string = loads(secret_data["SecretString"])
-        secret_string["basic.txt"] = "\n".join(
-            [secret_string["basic.txt"], str(api_key + ": " + api_secret + "," + proxy_user_string)]
-        )
-        location = str(secret_string["restProxyUsers.jaas"]).rfind("};")
-        if location == -1:
-            raise Exception("Could not parse/understand JAAS configs. Not sure what to do.")
-        secret_string["restProxyUsers.jaas"] = "\n".join(
-            [
-                secret_string["restProxyUsers.jaas"][:location],
-                "",
-                "\torg.apache.kafka.common.security.plain.PlainLoginModule required",
-                str('\tusername="' + api_key + '"'),
-                str('\tpassword="' + api_secret + '";'),
-                secret_string["restProxyUsers.jaas"][location:],
-            ]
-        )
-        update_secret(rest_proxy_secret_name, loads(secret_data["SecretString"]), secret_string, secret_tags)
-    return
-
-
-def run_aws_sec_mgr_workflow(
-    wf_type: str, api_keys: dict, add_as_rest_proxy_user: bool, secret_name_prefix: str, rp_fe_user_filepath: str
-):
-    for api_key in api_keys.values():
-        env_name = api_key["env_name"]
-        env_id = api_key["env_id"]
-        cluster_name = api_key["cluster_name"]
-        cluster_id = api_key["resource_id"]
-        sa_name = api_key["sa_name"]
-        sa_id = api_key["owner_resource_id"]
-        api_key_name = api_key["key"]
-        rest_proxy_user = "True" if add_as_rest_proxy_user else "False"
-        tags_for_create = {
-            "secret_type": "confluent_cloud",
-            "env_name": env_name,
-            "env_id": env_id,
-            "cluster_name": cluster_name,
-            "cluster_id": cluster_id,
-            "sa_name": sa_name,
-            "sa_id": sa_id,
-            "allowed_in_rest_proxy": rest_proxy_user,
-        }
-        secret_name = create_secret_name(env_id, cluster_id, sa_id, api_key_name, secret_name_prefix)
-        # client.restore_secret(SecretId=secret_name)
-        out_secret = get_secret(secret_name)
-        if "get" in wf_type:
-            if out_secret:
-                print("Secret Found.")
-                out = out_secret.copy()
-                out.pop("SecretString")
-                pp.pprint(out)
-                return out
-            else:
-                print("Could not find Secret with Name " + secret_name + " and workflow was retrieve only.")
-                return out_secret
-        if "store" in wf_type:
-            secret_data = {"username": api_key["key"], "password": api_key["secret"]}
-            if not out_secret:
-                create_secret(secret_name, secret_data, render_secret_tags_format(tags_for_create))
-            else:
-                update_secret(
-                    secret_name, out_secret["SecretString"], secret_data, render_secret_tags_format(tags_for_create)
-                )
-            if add_as_rest_proxy_user:
-                add_secrets_to_rest_proxy_user(
-                    env_id,
-                    cluster_id,
-                    secret_name_prefix,
-                    api_key["key"],
-                    api_key["secret"],
-                    rp_fe_user_filepath,
-                    tags_for_create,
-                    rest_proxy_user,
-                    proxy_user_string,
-                )
-
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(
-        description="Command line arguments for controlling the application",
-        add_help=True,
-    )
-
-    wf_args = parser.add_argument_group("workflow-args", "Workflow Selection Arguments")
-    wf_args.add_argument(
-        "--wf-name",
-        type=str,
-        default=[],
-        action="append",
-        dest="wf_name",
-        choices=["all", "create-sa", "get-sa", "create-apikey"],
-        help="Workflow that needs to be executed",
-    )
-
-    sa_args = parser.add_argument_group("sa-args", "Service Account Arguments")
-    sa_args.add_argument(
-        "--service-account-name",
-        type=str,
-        default=None,
-        help="Provide the name for which Service Account needs to be created.",
-    )
-    sa_args.add_argument(
-        "--force-new-account",
-        action="store_true",
-        default=False,
-        help="Force Generate a new Service Account even if an account exists with the same Name.",
-    )
-    sa_args.add_argument(
-        "--add-as-rest-proxy-user",
-        action="store_true",
-        default=False,
-        help="Add the Service Account to REST Proxy users",
-    )
-
-    api_args = parser.add_argument_group("api-args", "API management arguments")
-    api_args.add_argument(
-        "--setup-api-keys-for-clusters",
-        action="store_true",
-        default=False,
-        help="Generate new API Keys & Secrets while setting up the new Service Account",
-    )
-    api_args.add_argument(
-        "--create-service-account-if-necessary",
-        action="store_true",
-        default=False,
-        help="If enabled, will create a new Service Account when trying to create a new API Key. "
-        + "Otherwise, will throw an exception if Service Account is not found prior to creating the API Key.",
-    )
-    api_args.add_argument(
-        "--cluster-id",
-        type=str,
-        metavar="lkc-123456",
-        default=[],
-        action="append",
-        dest="cluster_list",
-        help="The Clusters list for which the api keys will be set up. ",
-    )
-    api_args.add_argument(
-        "--force-all-clusters",
-        action="store_true",
-        default=False,
-        help="If the API keys are needed for all available clusters, then --cluster-id could be ignored and this switch could be enabled.",
-    )
-    api_args.add_argument(
-        "--force-new-api-keys",
-        action="store_true",
-        default=False,
-        help="This will generate new API Keys even if there are existing API keys linked to a cluster already.",
-    )
-    api_args.add_argument(
-        "--generate-api-keys-output-file",
-        action="store_true",
-        default=False,
-        help="This will generate new API Keys even if there are existing API keys linked to a cluster already.",
-    )
-
-    sm_args = parser.add_argument_group("sm-args", "Secret Manager arguments")
-    sm_args.add_argument(
-        "--enable-secret-management",
-        action="store_true",
-        default=False,
-        help="This switch enables the Secret Management for API Keys and Service Accounts.",
-    )
-    sm_args.add_argument(
-        "--secret-manager-type",
-        type=str,
-        default="aws-secretmanager",
-        choices=["aws-secretmanager"],
-        help="Provide the Secret Storage Type to use",
-    )
-    api_args.add_argument(
-        "--secret-name-prefix",
-        type=str,
-        metavar="prefix_for_secret_name",
-        default=None,
-        help="If you need to add a prefix to the Secret name, it could be done with this switch.",
-    )
-
-    args = parser.parse_args()
-
-    base_ccloud.initial_setup(args.setup_api_keys_for_clusters)
-    api_keys = {}
-    with open("api_key_values.json", "r", encoding="utf-8") as input_file:
-        api_keys = loads(input_file.read())
-    run_aws_sec_mgr_workflow("store", api_keys, args.add_as_rest_proxy_user, args.secret_name_prefix)
-    print("")
+                if is_new_secret:
+                    env_details = ccloud_env_list.find_environment(cluster_details.env_id)
+                    secret_tags = self.__render_secret_tags(
+                        env_details.display_name,
+                        env_details.env_id,
+                        cluster_details.cluster_name,
+                        cluster_details.cluster_id,
+                        ccloud_sa_list.sa[rest_proxy_user.owner_id].name,
+                        rest_proxy_user.owner_id,
+                        True,
+                    )
+                    self.__create_secret(rp_secret_name, rp_secret_data, self.__render_secret_tags_format(secret_tags))
+                    self.add_to_cache(rp_secret_name, rp_secret_data, secret_tags)
+                else:
+                    response = client.put_secret_value(SecretId=rp_secret_name, SecretString=dumps(rp_secret_data))
+                    print(f"Secret Successfully updated. Response\n {response}")
