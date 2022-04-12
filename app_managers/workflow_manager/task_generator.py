5d5789b88987f2ee3d537e78396e3d48422beb64
Abhishek Walia <abhishek.walia@gmail.com>
2022-04-11 17:35:24 -0700

    Add Task for API Key Deletion management

--- app_managers/workflow_manager/task_generator.py
+++ app_managers/workflow_manager/task_generator.py
@@ -56,9 +56,16 @@ class CSMAPIKeyTasks(WorkflowTypes.CSMConfigDataMap):
     api_keys_in_ccloud: Set[str] = set()
     create_secrets_req: Set[str] = set()
     update_secrets_req: Set[str] = set()
+    secret_bundle: CSMSecretsManager
 
-    def __init__(self, csm_bundle: CoreTypes.CSMYAMLConfigBundle, ccloud_bundle: CCloudConfigBundle) -> None:
+    def __init__(
+        self,
+        csm_bundle: CoreTypes.CSMYAMLConfigBundle,
+        ccloud_bundle: CCloudConfigBundle,
+        secret_bundle: CSMSecretsManager,
+    ) -> None:
         super().__init__(csm_bundle=csm_bundle, ccloud_bundle=ccloud_bundle)
+        self.secret_bundle = secret_bundle
         self.refresh_set_values(csm_bundle=self.csm_bundle, ccloud_bundle=self.ccloud_bundle)
 
     def refresh_set_values(self, csm_bundle: CoreTypes.CSMYAMLConfigBundle, ccloud_bundle: CCloudConfigBundle):
@@ -77,12 +84,12 @@ class CSMAPIKeyTasks(WorkflowTypes.CSMConfigDataMap):
                 ]
             )
 
-    def create_api_key_tasks(self, secret_list: CSMSecretsManager):
+    def create_api_key_tasks(self):
         secrets_in_store = set()
         create_api_keys_req = self.find_items_to_be_created(
             config_item_names=self.api_keys_in_def, ccloud_item_names=self.api_keys_in_ccloud
         )
-        secrets_in_store.update(["~".join([v.sa_name, v.cluster_id]) for v in secret_list.secret.values()])
+        secrets_in_store.update(["~".join([v.sa_name, v.cluster_id]) for v in self.secret_bundle.secret.values()])
         self.create_secrets_req = self.find_items_to_be_created(
             config_item_names=self.api_keys_in_def, ccloud_item_names=secrets_in_store
         )
@@ -98,11 +105,11 @@ class CSMAPIKeyTasks(WorkflowTypes.CSMConfigDataMap):
         for item in create_api_keys_req:
             value = item.split("~", 1)
             sa_name, cluster_id = value[0], value[1]
-            sa_details = self.ccloud_bundle.cc_service_accounts.find_sa(sa_name)
             cluster_details = self.ccloud_bundle.cc_clusters.find_cluster(cluster_id)
-            api_keys = self.ccloud_bundle.cc_api_keys.find_keys_with_sa_and_cluster(
-                getattr(sa_details, "resource_id", None), cluster_details.cluster_id
-            )
+            # sa_details = self.ccloud_bundle.cc_service_accounts.find_sa(sa_name)
+            # api_keys = self.ccloud_bundle.cc_api_keys.find_keys_with_sa_and_cluster(
+            #     getattr(sa_details, "resource_id", None), cluster_details.cluster_id
+            # )
             yield WorkflowTypes.CSMConfigTask(
                 task_type=WorkflowTypes.CSMConfigTaskType.create_task,
                 object_type=WorkflowTypes.CSMConfigObjectType.api_key_type,
@@ -113,6 +120,33 @@ class CSMAPIKeyTasks(WorkflowTypes.CSMConfigDataMap):
                     "env_id": cluster_details.env_id,
                 },
             )
+
+    def delete_api_key_tasks(self):
+        ignore_sa_id_set = set(self.csm_bundle.csm_configs.ccloud.ignore_service_account_list)
+        ignore_sa_names = set(
+            [
+                item.name
+                for item in self.ccloud_bundle.cc_service_accounts.sa.values()
+                if item.resource_id in ignore_sa_id_set
+            ]
+        )
+        deletion_eligible_api_keys = self.find_items_to_be_deleted(
+            config_item_names=self.api_keys_in_def, ccloud_item_names=self.api_keys_in_ccloud
+        )
+        deletion_eligible_api_keys = set(
+            [item for item in deletion_eligible_api_keys if item.split("~", 1)[0] not in ignore_sa_names]
+        )
+        for item in deletion_eligible_api_keys:
+            value = item.split("~", 1)
+            sa_name, cluster_id = value[0], value[1]
+            sa_id = self.ccloud_bundle.cc_service_accounts.find_sa(sa_name=sa_name)
+            for key in self.ccloud_bundle.cc_api_keys.find_keys_with_sa_and_cluster(sa_id=sa_id, cluster_id=cluster_id):
+                yield WorkflowTypes.CSMConfigTask(
+                    task_type=WorkflowTypes.CSMConfigTaskType.delete_task,
+                    object_type=WorkflowTypes.CSMConfigObjectType.api_key_type,
+                    status=WorkflowTypes.CSMConfigTaskStatus.sts_not_started,
+                    task_object={"sa_name": sa_name, "sa_id": sa_id, "cluster_id": cluster_id, "api_key": key.api_key},
+                )
 
 
 class CSMSecretManagerTasks(WorkflowTypes.CSMConfigDataMap):
