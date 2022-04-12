7390091263c79f258e614ca5c46a2350484ae342
Abhishek Walia <abhishek.walia@gmail.com>
2022-04-12 10:44:33 -0700

    naming conventions update

--- app_managers/workflow_manager/main.py
+++ app_managers/workflow_manager/main.py
@@ -26,11 +26,11 @@ def trigger_workflows(args: Namespace):
         if csm_bundle.csm_configs.secretstore.store_type == CSMTypes.SUPPORTED_STORES.AWS_SECRETS:
             import secret_managers.aws_secrets_manager as aws_secrets_manager
 
-            secret_list = aws_secrets_manager.AWSSecretsList(csm_bundle=csm_bundle)
+            secret_bundle = aws_secrets_manager.AWSSecretsList(csm_bundle=csm_bundle)
         workflow_manager = WorkflowManager(
             csm_bundle=csm_bundle,
             ccloud_bundle=ccloud_bundle,
-            secret_bundle=secret_list,
+            secret_bundle=secret_bundle,
             dry_run=args.dry_run,
         )
         workflow_manager.create_service_accounts()
@@ -40,6 +40,6 @@ def trigger_workflows(args: Namespace):
                 workflow_manager.delete_api_keys()
             is_secret_updated = workflow_manager.update_api_keys_in_secret_manager()
             if is_secret_updated:
-                secret_list.create_update_rest_proxy_secrets(ccloud_bundle=ccloud_bundle, csm_bundle=csm_bundle)
+                secret_bundle.create_update_rest_proxy_secrets(ccloud_bundle=ccloud_bundle, csm_bundle=csm_bundle)
         if csm_bundle.csm_configs.ccloud.enable_sa_cleanup:
             workflow_manager.delete_service_accounts()
